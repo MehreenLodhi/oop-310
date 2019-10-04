a945c896406373b94c3e5b0223b02ebed4279df1
amehreen793 <amehreen793@gmail.com>
2019-10-04 10:51:20 -0700

    second commit

---
+++ assignment.py
@@ -0,0 +1,12 @@
+print('Hello world')
+print('You must be the legendary god that comes from the sky')
+print('We have been waiting for you for a long time')
+print('Our legend says you will be very tasty with mustard')
+print('We will have a feast tonight unless you say')
+
+x=43
+x=x+1
+print(x)
+print('I hate you python')
+print('Good bye')
+
